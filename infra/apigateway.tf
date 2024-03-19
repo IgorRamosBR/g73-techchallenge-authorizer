f993167d4450d0c8f4d331121485f4ab20a2815a
Alvaro <alvarocatellan@gmail.com>
2024-03-18 22:07:08 -0300

    provider-pipeline

---
+++ infra/apigateway.tf
@@ -0,0 +1,60 @@
+resource "aws_api_gateway_rest_api" "api_authorizer" {
+  name = "api_authorizer"
+  description = "Authorizer API"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api_authorizer.id
+  parent_id = aws_api_gateway_rest_api.api_authorizer.root_resource_id
+  path_part = "auth"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api_authorizer.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api_authorizer.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "GET"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.authorizer_lambda.invoke_arn
+}
+
+# resource "aws_api_gateway_method_response" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.my_api.id
+#   resource_id = aws_api_gateway_resource.root.id
+#   http_method = aws_api_gateway_method.proxy.http_method
+#   status_code = "200"
+
+# }
+
+# resource "aws_api_gateway_integration_response" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.my_api.id
+#   resource_id = aws_api_gateway_resource.root.id
+#   http_method = aws_api_gateway_method.proxy.http_method
+#   status_code = aws_api_gateway_method_response.proxy.status_code
+
+#   depends_on = [
+#     aws_api_gateway_method.proxy,
+#     aws_api_gateway_integration.lambda_integration
+#   ]
+# }
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.options_integration, # Add this line
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api_authorizer.id
+  stage_name = "production"
+}
---
+++ infra/lambda.tf
@@ -0,0 +1,49 @@
+resource "aws_lambda_function" "authorizer_lambda" {
+  s3_bucket = "g73-techchallenge-lambda-build"
+  s3_key = "authorizer.zip"
+  function_name = "api-authorizer"
+  role = aws_iam_role.lambda_role.arn
+  handler = "bin/main"
+  runtime = "go1.x"
+  environment {
+    variables = {
+      DB_HOST = "g73-techchallenge-db.cxokeewukuer.us-east-1.rds.amazonaws.com"
+      DB_PORT = "5432"
+      DB_USER = "g73_admin_user"
+      DB_PASSWORD = "UV6RetyeibtF"
+      DB_NAME = "techchallengedb"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+    {
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }
+  ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.authorizer_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api_authorizer.execution_arn}/*/*/*"
+}
+
